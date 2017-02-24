9845caa3558da0389a75b51357e5fcd5ea5a6dcd
David Galeano Corrales <dgaleano@bloomberg.net>
2017-02-24 14:51:39 +0000

    Updated to latest version from the nginx contrib repo Added script files to update

---
+++ update_from_source.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+curl -s -L -o ftdetect/nginx.vim https://github.com/nginx/nginx/raw/master/contrib/vim/ftdetect/nginx.vim
+curl -s -L -o ftplugin/nginx.vim https://github.com/nginx/nginx/raw/master/contrib/vim/ftplugin/nginx.vim
+curl -s -L -o indent/nginx.vim https://github.com/nginx/nginx/raw/master/contrib/vim/indent/nginx.vim
+curl -s -L -o syntax/nginx.vim https://github.com/nginx/nginx/raw/master/contrib/vim/syntax/nginx.vim
